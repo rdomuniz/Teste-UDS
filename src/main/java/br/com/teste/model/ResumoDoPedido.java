15534bbbb8c200e86a7569cee5a957728441c045
Rodrigo Muniz <rdomuniz@gmail.com>
2019-08-11 21:23:56 -0300

    tratamento de repetição em montagem de resumo!

--- src/main/java/br/com/teste/model/ResumoDoPedido.java
+++ src/main/java/br/com/teste/model/ResumoDoPedido.java
@@ -5,6 +5,7 @@ import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.teste.exception.ResolvedException;
 import br.com.teste.model.enums.Detalhe;
 import br.com.teste.model.enums.Personalizacoes;
 import br.com.teste.model.enums.Sabores;
@@ -29,6 +30,8 @@ public class ResumoDoPedido {
 		adciona(tamanho.getDetalhe());
 		adciona(sabor.getDetalhe());
 		for (Personalizacoes personalizacao : pedido.getPersonalizacoes()) {
+			if(personalizacoes.stream().anyMatch(p -> p.getPersonalizacao() == personalizacao))
+				throw new ResolvedException("Personalização '" + personalizacao + "' repetida!");
 			personalizacoes.add(new ResumoDaPersonalizacaoDoPedido(personalizacao));
 			adciona(personalizacao.getDetalhe());
 		}
